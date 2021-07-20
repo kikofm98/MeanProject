bc7562e57d670167853b825f55f5e27150aefe34
Francisco Fernández Montiel <0619645525@uma.es>
2021-07-20 20:22:50 +0200

    miau

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb+srv://admin:12345@cluster0.5elcn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' , {
+mongoose.connect('mongodb+srv://kikolla:<password>@cluster0.gr6zf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' , {
     useNewUrlParser:true, useUnifiedTopology: true, useCreateIndex:true
 })
 
